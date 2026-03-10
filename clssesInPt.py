ccc995899e974a2d35532dc5ed457dc972b44421
Arghya Ghosh <arghyaghosh790@gmail.com>
2026-03-10 14:15:50 +0530

    updated classInPt

--- clssesInPt.py
+++ clssesInPt.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO(r"visual_analytics_fastapi_backend\app\ML_models\latest_16_1_2026.pt")
+model = YOLO(r"app\ML_models\latest_16_1_2026.pt")
 
 print(model.names)
